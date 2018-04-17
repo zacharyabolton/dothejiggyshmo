954c1b07ec61d4e07f65bcdd4b1363fb890f3a39
Zac Bolton <zacbolton2129@gmail.com>
2018-04-17 17:34:23 -0300

    basic structure + ryw:blog

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,9 +1,23 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+// Import needed templates
+import '../../ui/layouts/body/body.js';
+import '../../ui/pages/not_found/not_found.js';
+import '../../ui/pages/home/home.js';
+import '../../ui/pages/about/about.js';
 
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
     BlazeLayout.render('App_body', { main: 'App_home' });
+  },
+});
+
+FlowRouter.route('/about', {
+  name: 'App.about',
+  action() {
+    BlazeLayout.render('App_body', { main: 'App_about' });
   },
 });
 
---
+++ imports/ui/layouts/body/body.js
@@ -0,0 +1,4 @@
+import './body.html';
+import '../../components/banner/banner.js';
+
+document.addEventListener("touchstart", function(){}, true); // added from old project. not really understood.
