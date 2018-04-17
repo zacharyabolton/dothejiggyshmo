e28709aa84e8dba424b70b7d463dfd51da9a6dea
Zac Bolton <zacbolton2129@gmail.com>
2018-04-17 19:49:24 -0300

    reverting to simpler architecture

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,4 +1,5 @@
 import './body.html';
 import '../../components/banner/banner.js';
+import '../../components/footer/footer.js';
 
 document.addEventListener("touchstart", function(){}, true); // added from old project. not really understood.
--- server/blog.js
+++
@@ -1,4 +0,0 @@
-Blog.config({
-	rss: 'Do the Jiggy Shmo',
-	description: 'My Blog <in develpment>',
-});
