aa2540bb68582b024aa6145bf3d101c9a825165b
Zac Bolton <zacbolton2129@gmail.com>
2018-10-17 18:43:34 -0300

    single post page functionality implemented & go home btn

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -7,74 +7,59 @@ import './home.html';
 
 
 Template.App_home.onCreated( () => {
+
     let template = Template.instance();
 
-    // set the limit on posts to 1 initially
     template.requestedPostNumber = new ReactiveVar();
     template.requestedPostNumber.set(1)
-    //
 
-    // subsribe to one or all posts depending on
-    // whether or not a slug is in the URL
-    slug = Router.current().params.slug;
-    if ( slug ) {
-        template.autorun( () => {
-            template.subscribe( 'onepost', slug );
-        });
-    } else {
-        template.autorun( () => {
-            console.log(template.requestedPostNumber.get())
-            template.subscribe( 'allposts', template.requestedPostNumber.get() );
-        });
-    }
-    //
+    template.allposts = new ReactiveVar();
+    template.allposts.set(true)
 
-    // // detect if scroll is happening at bottom of page
-    // // in order to load next doc.
-    // lastScrollTop = 0
+    template.slug = new ReactiveVar();
+    template.slug.set(Router.current().params.slug)
 
-    // $( window ).scroll( function( event ) {
-        
-    //     if ( $( window ).scrollTop() + $( window ).height() > $( document ).height() - 10 ) {
-    //         // where we are in the page?
-    //         var scrollTop = $(this).scrollTop();
-    //         // console.log(new Date())
-    //         if ( scrollTop > lastScrollTop ) {
-    //             var requestedPostNumber = template.requestedPostNumber.get();
-    //             template.requestedPostNumber.set(requestedPostNumber += 1)
-    //             // console.log(template.requestedPostNumber.get())
-    //             // console.log("going down at the bottom of the page");
-    //         }
-    //         lastScrollTop = scrollTop
-    //     }
-    //     console.log("window scrollTop is "+$( window ).scrollTop())
-    //     console.log("window height is "+$( window ).height())
-    //     console.log("document height is "+$( document ).height())
-    //     console.log("scrollTop is "+scrollTop)
-    //     console.log("lastScrollTop is "+lastScrollTop)
-    // })
-    // //
+    template.autorun( () => {
+        if ( template.slug.get() ) {
+            template.allposts.set(false)
+                template.subscribe( 'onepost', template.slug.get() );
+        } else {
+            template.allposts.set(true)
+                template.subscribe( 'allposts', template.requestedPostNumber.get() );
+        }
+    });
 
 });
 
 Template.App_home.helpers({
     posts() {
-        let post = Posts.find({}) 
+        let post = Posts.find({});
         if ( post ) {
             return post;
         } else {
             console.log('no post found!!!!!!')
         }
+    },
+    allposts() {
+        return Template.instance().allposts.get();
     }
 });
 
 Template.App_home.events({
     'click .glyphicon-chevron-down' (event, template) {
+        event.preventDefault();
         let requestedPostNumber = template.requestedPostNumber.get();
         template.requestedPostNumber.set(requestedPostNumber += 1)
     },
     'click .glyphicon-chevron-up' (event, template) {
+        event.preventDefault();
         let requestedPostNumber = template.requestedPostNumber.get();
         template.requestedPostNumber.set(requestedPostNumber -= 1)
+    },
+    'click .return-home' ( event, template ) {
+        event.preventDefault();
+        Template.instance().slug.set();
+        Template.instance().allposts.set(true);
+        Template.instance().requestedPostNumber.set(1);        
     }
 })
